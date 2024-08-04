2e2e7ad4df3a2089ce6dbc920bfe2ce6cc0df1d6
scnetter <36lima@gmail.com>
2024-08-04 14:28:34 -0500

    small functional contract

--- welcome-to-solidity.sol
+++ welcome-to-solidity.sol
@@ -1,5 +1,14 @@
 pragma solidity >= 0.7.0 < 0.9.0;
 
-contract ThisIsAContract {
-    
+// SPDX-License-Identifier: UNLICENSED
+
+contract WelcomeToSolidity {
+    constructor() {
+    }
+    function getResult() public pure returns(uint){
+        uint a = 1;
+        uint b = 2;
+        uint result = a + b;
+        return result;
+    }
 }
