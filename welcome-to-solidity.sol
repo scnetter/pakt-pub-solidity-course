85e18c88c312aa049829e6fb2c808e6b9cb78bbd
scnetter <36lima@gmail.com>
2024-08-04 14:36:51 -0500

    Small variable change

--- welcome-to-solidity.sol
+++ welcome-to-solidity.sol
@@ -6,8 +6,8 @@ contract WelcomeToSolidity {
     constructor() {
     }
     function getResult() public pure returns(uint){
-        uint a = 1;
-        uint b = 2;
+        uint a = 10;
+        uint b = 5;
         uint result = a + b;
         return result;
     }
